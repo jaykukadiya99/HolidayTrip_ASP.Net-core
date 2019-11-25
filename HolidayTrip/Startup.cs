52d63615f41eac8dc7139726230cc8b1e1a07f7a
yatinjpatel <yatinjpatel30@gmail.com>
2019-11-25 09:42:41 +0530

    cors error solve

--- HolidayTrip/Startup.cs
+++ HolidayTrip/Startup.cs
@@ -23,23 +23,23 @@ namespace HolidayTrip
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddControllersWithViews();
-
-             services.AddCors(option =>
+            services.AddCors(option =>
             {
                 option.AddDefaultPolicy(
                     builder =>
                     {
                         builder.AllowAnyOrigin()
                         .AllowAnyMethod()
-                        .AllowAnyHeader();                     
+                        .AllowAnyHeader();
                     });
             });
+            services.AddControllersWithViews();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            app.UseCors();
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
